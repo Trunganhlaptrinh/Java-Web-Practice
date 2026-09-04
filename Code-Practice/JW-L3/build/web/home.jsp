8803c5d380ad91000c7d9a05e600362682419034
Tranh A Nguyen <80273857+Trunganhlaptrinh@users.noreply.github.com>
2026-09-04 15:12:14 +0700

    update practice L3 + note

---
+++ Code-Practice/JW-L3/build/web/home.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : home
+    Created on : Sep 3, 2026, 4:45:32 PM
+    Author     : Trung Anh
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Trung Anh Nguyen</h1>
+        
+        <!-- muốn hiển thị giá trị thì dùng $ và {giá trị}  -->
+        <h2>${name}</h2>
+        <h2>${number}</h2>
+<!--        <h2>${message}</h2>-->
+    </body>
+</html>
---
+++ Code-Practice/JW-L3/src/java/controller/HomeServlet.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Trung Anh
+ */
+@WebServlet(name = "HomeServlet", urlPatterns = {"/home"})
+public class HomeServlet extends HttpServlet {
+
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HomeServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet HomeServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        int a = 10;
+        request.setAttribute("number", a);
+        request.setAttribute("name", "Trung Anh");
+        // thêm cái này chỉ dùng dc doGet k thể dopost
+        
+        request.getRequestDispatcher("home.jsp").forward(request, response);
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+    
+
+}
